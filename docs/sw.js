62312bf0eb2142d717ea3a2ce214c30c9328a422
DannyPrototype <116859731+DannyPrototype@users.noreply.github.com>
2026-08-24 17:21:18 -0500

    Agregar docs/sw.js

---
+++ docs/sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'calculadora-feliz-v1';
+const ARCHIVOS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))));
+
